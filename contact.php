5976d5c78202058938dda52db9b79e5d1a28459a
Bernard Bolter <b@bernardbolter.com>
2014-06-24 18:32:57 -0700

    design of large screen version

--- contact.php
+++ contact.php
@@ -1,10 +1,15 @@
 <div class="contact_rap" id="contact">
 	<div class="contact_container">
-		<a href="#"><img src="<?php bloginfo( 'template_url' ); ?>/gfx/F_icon.png"> Visit us on Facebook</a>
-		<h3>Writers:</h3>
-		<p><b>Ed Cupman</b> - edcupman@me.com</p>
-		<p><b>Bernie Bolter</b> - berniebolter@gmail.com</p>
-		<p><b>Craig Cummins</b> - craigpsych@aol.com</p>
-		<h3>Contact Form:</h3>
+		<div class="contact_left">
+			<a href="#"><img src="<?php bloginfo( 'template_url' ); ?>/gfx/F_icon.png"> Visit &nbsp; us &nbsp; on &nbsp; Facebook</a>
+			<h3>Writers:</h3>
+			<p><b>Ed Cupman</b> - edcupman@me.com</p>
+			<p><b>Bernie Bolter</b> - berniebolter@gmail.com</p>
+			<p><b>Craig Cummins</b> - craigpsych@aol.com</p>
+		</div> <!-- contact left -->
+		<div class="contact_right">
+			<h3>Contact &nbsp; Form:</h3>
+			<?php echo do_shortcode( '[contact-form-7 id="4" title="Contact form 1"]' ); ?>
+		</div> <!-- contact right -->
 	</div> <!-- contact container -->
 </div> <!-- contact rap -->
--- video.php
+++ video.php
@@ -1,8 +1,9 @@
-<div class="video_rap">
-	<div class="video_container">
-		<h3>Latest Video</h3>
-		<div class="video_rapper">
-			<iframe width="560" height="315" src="//www.youtube.com/embed/1Pv9FWTv8Zg" frameborder="0" allowfullscreen></iframe>
-		</div> <!-- video_rapper -->
-	</div> <!-- video container -->
-</div> <!-- video rap -->
+	<div class="video_rap">
+		<div class="video_container">
+			<h3>Latest Video</h3>
+			<div class="video_rapper">
+				<iframe width="560" height="315" src="//www.youtube.com/embed/1Pv9FWTv8Zg" frameborder="0" allowfullscreen></iframe>
+			</div> <!-- video_rapper -->
+		</div> <!-- video container -->
+	</div> <!-- video rap -->
+</div> <!-- large container -->
